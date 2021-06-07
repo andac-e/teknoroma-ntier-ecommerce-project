3bb0926a95469bd1e8773bff765bbe6586a1a054
andac-e <65905654+andac-e@users.noreply.github.com>
2021-06-07 17:41:15 +0300

    DAL katmanında MyContext sınıfı oluşturuldu.

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Context
+{
+    using MAP.Options;
+    using ENTITIES.Models;
+    using DAL.StrategyPattern;
+    using System.Data.Entity;
+
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new EmployeeMap());
+            modelBuilder.Configurations.Add(new EmployeeTerritoryMap());
+            modelBuilder.Configurations.Add(new ExpenseMap());
+            modelBuilder.Configurations.Add(new IssueMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new OrderDetailMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new ShipperMap());
+            modelBuilder.Configurations.Add(new SupplierMap());
+            modelBuilder.Configurations.Add(new TerritoryMap());
+            modelBuilder.Configurations.Add(new UserProfileMap());
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Territory> Territories { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
