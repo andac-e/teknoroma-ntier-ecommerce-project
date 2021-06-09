891a296fad110caffe2edd840b4db5c832a2e692
andac-e <65905654+andac-e@users.noreply.github.com>
2021-06-09 12:54:45 +0300

    Authentication classlar oluşturuldu. Layout sayfası oluşturuldu.

---
+++ Project.WEBUI/AuthenticationClasses/EmployeeAuthentication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WEBUI.AuthenticationClasses
+{
+    public class EmployeeAuthentication:AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["manager"] != null || 
+                httpContext.Session["sale"] != null || 
+                httpContext.Session["ware"] != null || 
+                httpContext.Session["acc"] != null || 
+                httpContext.Session["tech"] != null || 
+                httpContext.Session["mobile"] != null)
+            {
+                return true;
+            }
+            httpContext.Response.Redirect("/Home/Login");
+            return false;
+
+        }
+    }
+}
