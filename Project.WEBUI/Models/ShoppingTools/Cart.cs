8edc2a426cba32a429466aad9f1d826d44632d3d
andac-e <65905654+andac-e@users.noreply.github.com>
2021-06-12 11:17:40 +0300

    ShoppingTools eklendi.

--- Project.WEBUI/Models/ShoppingTools/Cart.cs
+++ Project.WEBUI/Models/ShoppingTools/Cart.cs
@@ -7,5 +7,46 @@ namespace Project.WEBUI.Models.ShoppingTools
 {
     public class Cart
     {
+
+        Dictionary<int, CartItem> _sepetim;
+
+        public Cart()
+        {
+            _sepetim = new Dictionary<int, CartItem>();
+        }
+
+        public List<CartItem> Sepetim { get { return _sepetim.Values.ToList(); } }
+
+        //Sepete Ekleme
+        public void AddToCart(CartItem item)
+        {
+            if (_sepetim.ContainsKey(item.ID))
+            {
+                _sepetim[item.ID].Amount += 1;
+                return;
+            }
+            _sepetim.Add(item.ID, item);
+        }
+
+        //Sepetten Çıkarma
+        public void RemoveFromCart(int id)
+        {
+            if (_sepetim[id].Amount>1)
+            {
+                _sepetim[id].Amount -= 1;
+                return;
+            }
+            _sepetim.Remove(id);
+        }
+
+        //Sepet Toplam Fiyatı
+        public decimal TotalPrice { get { return _sepetim.Sum(x => x.Value.SubTotal); } }
+
+        //Sepetteki Ürün Sayısı
+        public int ProductCount()
+        {
+            return _sepetim.Sum(x => x.Value.Amount);
+        }
+
     }
 }
--- Project.WEBUI/Models/ShoppingTools/CartItem.cs
+++ Project.WEBUI/Models/ShoppingTools/CartItem.cs
@@ -7,5 +7,16 @@ namespace Project.WEBUI.Models.ShoppingTools
 {
     public class CartItem
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public short Amount { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        public decimal SubTotal { get { return Price * Amount; } }
+
+        public CartItem()
+        {
+            Amount++;
+        }
     }
 }
